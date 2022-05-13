9d30bafcc063bad55cfb24e49e2acde231b136ec
jinwook song <wlsdnr129@naver.com>
2022-05-13 21:32:21 +0900

    screenshot

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+KEWWORD = "buy domain"
+
+
 # option to be kept open
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
+    service=Service(ChromeDriverManager().install()), options=None
 )
 driver.get("https://google.com")
 
 search_bar = driver.find_element_by_class_name("gLFyf")
-search_bar.send_keys("hello")
+search_bar.send_keys(KEWWORD)
 search_bar.send_keys(Keys.ENTER)
 
 search_results = driver.find_elements_by_class_name("g")
 
-for search_result in search_results:
-    title = search_result.find_element_by_tag_name("h3")
-    if title:
-        print(title.text)
+for idx, search_result in enumerate(search_results):
+    search_result.screenshot(f"screenshots/{KEWWORD}_{idx}.png")
 
 
 driver.quit()
